bd85645167b4fbafd05218622d99a6fb472db212
siva-enfono <siva@enfono.in>
2026-01-27 22:51:14 +0300

    Revert "job_type"

--- ksa_logistics/ksa_logistics/doctype/invoice_item/invoice_item.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, ramees@enfono.com and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class InvoiceItem(Document):
-	pass
